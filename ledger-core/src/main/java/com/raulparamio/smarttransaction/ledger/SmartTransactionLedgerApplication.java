d3179cfd698b73b6c72b1f12ba85ce65d1127cbd
RaulParamio <raulparamio1@gmail.com>
2026-01-07 23:35:36 +0100

    Initial commit

---
+++ ledger-core/src/main/java/com/raulparamio/smarttransaction/ledger/SmartTransactionLedgerApplication.java
@@ -0,0 +1,13 @@
+package com.raulparamio.smarttransaction.ledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTransactionLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTransactionLedgerApplication.class, args);
+	}
+
+}
---
+++ ledger-core/src/test/java/com/raulparamio/smarttransaction/ledger/SmartTransactionLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raulparamio.smarttransaction.ledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTransactionLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
